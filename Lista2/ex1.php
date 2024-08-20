f9587256376b959751939b8d210ba56bb86fc855
vinithales <127968652+vinithales@users.noreply.github.com>
2024-08-20 20:29:21 -0300

    Final da aula

--- Lista2/ex1.php
+++ Lista2/ex1.php
@@ -7,8 +7,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Exercicio 1</h1>
-    <form action="exer1resp.php" method="post">
+    <h1 class="ms-5 mt-5">Exercicio 1</h1>
+    <form action="exer1resp.php" method="post" class="m-5">
         <div class="row">
             <div class="col">
               <label for="valor1" class="form-label">Informe o valor 1: </label>
@@ -19,9 +19,9 @@
                 <input type="number" class="form-control"name="valor2" id="valor2">
             </div>
         </div>
-        <div class="row">
+        <div class="row ">
             <div class="col">
-                <button type="submit" class="btn btn-danger">Enviar</button>
+                <button type="submit" class="btn btn-danger mt-3">Enviar</button>
             </div>
         </div>
 
