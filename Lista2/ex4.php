9ec46d526a48238640203933dd03f8584efbc51f
vinithales <127968652+vinithales@users.noreply.github.com>
2024-08-24 11:05:48 -0300

    metade do exercicio 4

---
+++ Lista2/ex4.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Exercicio 4</h1>
+    <form action="exer4resp.php" class="m-5">
+        <div class="row">
+            <div class="col">
+                <label for="valor1" class="form-label">Infome o valor 1: </label>
+                <input type="number" name="valor1" id="valor1" class="form-control">
+            </div>
+            <div class="col">
+                <label for="valor2" class="form-label">Informe o valor 2: </label>
+                <input type="number" name="valor2" id="valor2" calss="form-control">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <button type="submit" class="btn btn-danger">
+                    Enviar
+                </button>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
