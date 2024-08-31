c7269ae5a6e9746c5270472100caafb28544fcdf
vinithales <127968652+vinithales@users.noreply.github.com>
2024-08-31 10:28:58 -0300

    incompleto

--- Lista3/ex1.php
+++ Lista3/ex1.php
@@ -7,6 +7,17 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <form action="resposta1.php" method="POST">
+        <?php 
+            for($i = 1; $i <= 7; $i++)
+            {
+            ?>
+            <input type="number" name="valor <?= $i?>">
+            <?php
+
+                }
+            ?>
+        <button type="submit"> Enviar</button>
+    </form>
 </body>
 </html>
