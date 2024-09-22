f9e40b9cdd073b3f8261aaac1c22ed9b70330974
Vinícius Thales <viniciusthales486@gmail.com>
2024-09-22 03:15:22 -0300

    começo lista5

--- Aula4/form_mapas.php
+++ Aula4/form_mapas.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <form action="" method="POST">
+    <form action="mapas.php" method="POST">
         <?php for($i=1; $i<= 10;$i++): ?>
             <input type="text" name="nomes[]" class="form-control" placeholder="Valor <?= $i?>" />
         <?php endfor; ?>
