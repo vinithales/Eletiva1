785aa3cf3f7d23c457b21b32d4cb0aa01e7838fe
Vinícius Thales <viniciusthales486@gmail.com>
2024-09-22 14:24:06 -0300

    completo

--- Lista5/ex1.php
+++ Lista5/ex1.php
@@ -1,24 +1,48 @@
 <!doctype html>
-<html lang="en">
+<html lang="pt-br">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Exercício 2</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
-  <body>
-    <main class="container">
-        <form action="ex1resp.php" method="post">
-            <label for="nome" class="form-label"> Informe o Nome: </label>
-            <?php for( $i =1; $i <= 5; $i++) : ?>
-                <input type="text" name="nomes[]" class="form-control" placeholder="nome <?= $i?>" />
-                <input type="text" name="telefone[]" class="form-control" placeholder="telefone <?= $i?>" />
-            <?php endfor; ?>
-            <button type="submit" class="btn btn-danger">
-            Enviar
-        </button>
-        </form>
-    </main>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  </body>
-</html>
+<body>
+    <form action="" method="POST">
+        <?php for($i=1;$i<=5;$i++):?>
+        <div class="container">
+          <div class="row">
+            <label for="palavra" class="form-label">Informe o nome e número <?= $i ?> </label>
+              <div class="col">
+                  <input type="text" name="nomes[]" placeholder="Nome <?= $i ?>"/>
+              </div>
+              <div class="col">
+                  <input type="number" name="numeros[]" placeholder="Número <?= $i ?>"/>
+              </div>
+          </div>
+        </div>
+        <?php endfor; ?>
+        <div class="container">
+            <button type="submit">Enviar</button>
+        </div>
+
+        <?php
+            if ($_SERVER['REQUEST_METHOD'] == "POST"){
+                try{
+
+                    $nome = $_POST['nomes'];
+                    $numero = $_POST['numeros'];
+                    $nomesNumeros = [];
+                    foreach ($nome as $chave => $valor)
+                    {
+                        if ((!isset($nomesNumeros[$valor]) && (!in_array($numero[$chave],$nomesNumeros))))
+                        $nomesNumeros[$valor] = $numero[$chave];
+                    }
+                    print_r($nomesNumeros);
+                } catch (Exception $e){
+                    echo $e->getMessage();
+                }
+            }
+        ?>
+
+    </form>
+</body>
